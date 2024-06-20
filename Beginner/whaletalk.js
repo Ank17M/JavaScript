e21f3cc0f4f9b42a855128a4417396b5cf04a974
Ankit Mourya <arccra3@gmail.com>
2024-06-20 08:10:59 +0530

    whale talk

---
+++ Beginner/whaletalk.js
@@ -0,0 +1,25 @@
+let input = 'turpentine and turtles';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const resultArray = [];
+for(let i = 0; i < input.length; i++){
+  //console.log(input[i]);
+  //console.log(i);
+  input = input.toLowerCase();
+  if(input[i] === 'e'){
+    resultArray.push(input[i]);
+  }
+  if(input[i] === 'u'){
+    resultArray.push(input[i]);
+  }
+  for(let j = 0; j < vowels.length; j++){
+    //console.log(vowels[j]);
+    //console.log(j);
+    if(input[i] === vowels[j]){
+      resultArray.push(input[i]);
+    }
+  }
+}
+console.log(resultArray);
+let resultString = resultArray.join('');
+resultString = resultString.toUpperCase();
+console.log(resultString);
