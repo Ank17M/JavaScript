ff8e7515932bacdb31acb3186f1a829ec0a7a45a
Ankit Mourya <arccra3@gmail.com>
2024-06-22 07:26:15 +0530

    High Order Function

---
+++ Beginner/functionsasparametes.js
@@ -0,0 +1,19 @@
+//A higher-order function is a function that either accepts functions as parameters, returns a function, or both! 
+//We call functions that get passed in as parameters callback functions. 
+//Callback functions get invoked during the execution of the higher-order function.
+const addTwo = num => {
+    return num + 2;
+  }
+  
+  const checkConsistentOutput = (func, val) => {
+    let checkA = val + 2;
+    let checkB = func(val);
+    if(checkA === checkB){
+      return func(val);
+    } else {
+      return 'inconsistent results';
+    }
+  }
+  
+  console.log(checkConsistentOutput(addTwo, 7));
+  
