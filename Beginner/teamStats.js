2994c465a277a0405c4c4dac2dda6da49762c61b
Ankit Mourya <arccra3@gmail.com>
2024-06-28 09:50:42 +0530

    Program using Objects

---
+++ Beginner/teamStats.js
@@ -0,0 +1,63 @@
+const team = {
+    _players: [{
+      firstName: 'Ankit',
+      lastName: 'Mourya',
+      age: 22
+    },
+    {
+      firstName: 'Badal',
+      lastName: 'Kushwaha',
+      age: 23
+    },
+    {
+      firstName: 'Aniket',
+      lastName: 'Chauhan',
+      age: 23
+    }],
+    _games: [{
+      opponent: 'random1',
+      teamPoints: 11,
+      opponentPoints: 11
+    },
+    {
+      opponent: 'random2',
+      teamPoints: 12,
+      opponentPoints: 12
+    },
+    {
+      opponent: 'random3',
+      teamPoints: 13,
+      opponentPoints: 13
+    }],
+    get players() {
+      return this._players;
+    },
+    get games() {
+      return this._games;
+    },
+    addPlayer(newfirstName, newLastName, newAge){
+      const player = {
+      firstName: newfirstName,
+      lastName: newLastName,
+      age: newAge
+      };
+      this._players.push(player);
+    },
+    addGame(newOpponent, newTeamPoints, newOpponentPoints){
+      const game = {
+        opponent: newOpponent,
+        teamPoints: newTeamPoints,
+        opponentPoints: newOpponentPoints
+      };
+      this._games.push(game);
+    }
+  };
+  
+  team.addPlayer('Bugs', 'Bunny', 76);
+  team.addGame('Titans', 100, 98);
+  //Test
+  //console.log(team._players);
+  //console.log(team._games);
+  //console.log(team);
+  console.log(team.players);
+  console.log(team.games);
