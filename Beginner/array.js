0aef2b77c6d454c213803af697d31aae87878ff3
Ankit Mourya <arccra3@gmail.com>
2024-07-12 08:07:32 +0530

    "Removed empty line, popped last element, pushed two new elements, replaced element at index 7, and shifted first element in secretMessage array."

--- Beginner/array.js
+++ Beginner/array.js
@@ -1,7 +1,6 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 secretMessage.pop();
 secretMessage.push('to', 'program');
-
 secretMessage[7] = 'right';
 
 secretMessage.shift();
