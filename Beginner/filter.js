f9f6c4cd2739db32f9710523535fb3149c31094f
Ankit Mourya <arccra3@gmail.com>
2024-07-13 10:48:18 +0530

    "Removed unnecessary line from filter.js"

--- Beginner/filter.js
+++ Beginner/filter.js
@@ -1,6 +1,5 @@
 const randomNumbers = [375, 200, 3.14, 7, 13, 852];
 
-// Call .filter() on randomNumbers below
 const smallNumbers = randomNumbers.filter(randomNumber => {
   return randomNumber < 250;
 });
