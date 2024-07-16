92a4a8573375d03985eb2c66f235e74ffe060b71
Ankit Mourya <arccra3@gmail.com>
2024-07-16 09:27:54 +0530

    "Updated comment in map.js"

--- Beginner/map.js
+++ Beginner/map.js
@@ -1,6 +1,6 @@
 const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-// Create the secretMessage
+// Create the
 const secretMessage = animals.map(animal => {
   return animal[0];
 });
