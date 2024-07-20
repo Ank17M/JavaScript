bfc824f60493aaefbc4b41c4aa2c4d49d94e0ac6
Ankit Mourya <arccra3@gmail.com>
2024-07-20 18:16:49 +0530

    "Removed comment and added whitespace in objectbracketaccess.js"

--- Beginner/objectbracketaccess.js
+++ Beginner/objectbracketaccess.js
@@ -7,7 +7,7 @@ let spaceship = {
   
   let propName =  'Active Mission';
 
-  //
+  
   let isActive = spaceship['Active Mission'];
   console.log(isActive);
   console.log(spaceship['Active Mission']);
