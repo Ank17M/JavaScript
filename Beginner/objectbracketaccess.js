f492085cd11325804dd72d1e2c230cf7944169e4
Ankit Mourya <arccra3@gmail.com>
2024-07-20 18:16:41 +0530

    "Removed 'Write' comment and added empty comment in objectbracketaccess.js"

--- Beginner/objectbracketaccess.js
+++ Beginner/objectbracketaccess.js
@@ -7,7 +7,7 @@ let spaceship = {
   
   let propName =  'Active Mission';
 
-  // Write
+  //
   let isActive = spaceship['Active Mission'];
   console.log(isActive);
   console.log(spaceship['Active Mission']);
