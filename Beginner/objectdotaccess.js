3ba5422e749ea852cda6661f392315abc0e1e857
Ankit Mourya <arccra3@gmail.com>
2024-07-21 00:54:49 +0530

    "Fixed syntax error: changed closing bracket to closing brace in spaceship object definition."

--- Beginner/objectdotaccess.js
+++ Beginner/objectdotaccess.js
@@ -4,7 +4,7 @@ let spaceship = {
     'Fuel Type': 'Turbo Fuel',
     numCrew: 5,
     flightPath: ['Venus', 'Mars', 'Saturn']
-  };
+};
   
   const crewCount = spaceship.numCrew;
   const planetArray = spaceship.flightPath;
