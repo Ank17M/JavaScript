57f58671a02b05c88e0ce37bd78384d0267a7540
Ankit Mourya <arccra3@gmail.com>
2024-07-22 11:09:17 +0530

    "Fixed typo in comment: 'rightoterator' -> 'rotator' not made, but a comment was reformatted."

--- Beginner/rightoterator.js
+++ Beginner/rightoterator.js
@@ -8,7 +8,7 @@ cities.forEach(city => console.log('Have you visited ' + city + '?'));
 //Choose a method that will return a new array
 const longCities = cities.filter(city => city.length > 7);
 
-// Choose a method that will return a single value
+//Choose a method that will return a single value
 const word = cities.reduce((acc, currVal) => {
   return acc + currVal[0]
 }, "C");
