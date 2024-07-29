d967197a8cd7e3c945c52be9936c8db5f7f04d78
Ankit Mourya <arccra3@gmail.com>
2024-07-29 09:14:48 +0530

    "Removed two comment lines and no other changes."

--- Beginner/someevery.js
+++ Beginner/someevery.js
@@ -8,8 +8,6 @@ const interestingWords = words.filter(word => {
   return word.length > 5;
 });
 
-// Make sure to uncomment the code below and fix
-
 console.log(interestingWords.every((word) => {
   return word.length > 5;
 } ));
