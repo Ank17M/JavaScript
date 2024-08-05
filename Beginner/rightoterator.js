1cf6ba8336b01f90b403cb7af3da9f9fe11c2566
Ankit Mourya <arccra3@gmail.com>
2024-08-05 19:32:47 +0530

    "Updated comments in rightoterator.js"

--- Beginner/rightoterator.js
+++ Beginner/rightoterator.js
@@ -4,7 +4,7 @@ const nums = [1, 50, 75, 200, 350, 525, 1000];
 
 cities.forEach(city => console.log('Have you visited ' + city + '?'));
 
-//Choose a method
+//Choose
 const longCities = cities.filter(city => city.length > 7);
 
 //Choose a method that will return a single value
