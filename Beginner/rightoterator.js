f9609e46d121c6cda6744f6ce97cf9a20dba384d
Ankit Mourya <arccra3@gmail.com>
2024-08-06 19:49:08 +0530

    "Updated comment in rightoterator.js"

--- Beginner/rightoterator.js
+++ Beginner/rightoterator.js
@@ -6,7 +6,7 @@ cities.forEach(city => console.log('Have you visited ' + city + '?'));
 
 const longCities = cities.filter(city => city.length > 7);
 
-//Choose a method that will return
+//Choose a method that
 const word = cities.reduce((acc, currVal) => {
   return acc + currVal[0]
 }, "C");
