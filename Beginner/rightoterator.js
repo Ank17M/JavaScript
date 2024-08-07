1aa4a3aedcb7484d57ee9fcb05112a5809a25279
Ankit Mourya <arccra3@gmail.com>
2024-08-07 15:09:29 +0530

    "Updated comments in rightoterator.js"

--- Beginner/rightoterator.js
+++ Beginner/rightoterator.js
@@ -11,7 +11,7 @@ const word = cities.reduce((acc, currVal) => {
 }, "C");
 console.log(word)
 
-//Choose a method that will return a
+//Choose a method that will return
 const smallerNums = nums.map(num => num - 5);
 
 //Choose a method that will return a boolean value
