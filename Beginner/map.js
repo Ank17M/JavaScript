16a48496d10b144f86004cc765bc0b49568ed744
Ankit Mourya <arccra3@gmail.com>
2024-08-14 10:14:15 +0530

    "Updated map.js: changed comment and implemented map function to divide bigNumbers by 100"

--- Beginner/map.js
+++ Beginner/map.js
@@ -8,7 +8,7 @@ console.log(secretMessage.join(''));
 
 const bigNumbers = [100, 200, 300, 400, 500];
 
-//
+//Creating 
 const smallNumbers = bigNumbers.map(bigNumber => {
   return bigNumber/100;
 });
