135285fd172fc63de9dd368f7513bf180715fe48
Ankit Mourya <arccra3@gmail.com>
2024-08-16 09:53:10 +0530

    "Updated comment and added newline to end of file in foreach.js"

--- Beginner/foreach.js
+++ Beginner/foreach.js
@@ -1,4 +1,4 @@
 const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
-//Creating a foreach for 
+//Creating a foreach for data 
 fruits.forEach(fruitItem => console.log('I want to eat a ' + fruitItem));
