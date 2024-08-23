16f12c5fb0b08b19bbb2c21ae127628beea51f11
Ankit Mourya <arccra3@gmail.com>
2024-08-23 17:01:03 +0530

    .

--- Beginner/passbyreference.js
+++ Beginner/passbyreference.js
@@ -11,7 +11,7 @@ let spaceship = {
     obj['disabled'] = true;
   };
   
-  //Calling the 
+  //Calling the object
   console.log(spaceship);
   greenEnergy(spaceship);
   remotelyDisable(spaceship);
