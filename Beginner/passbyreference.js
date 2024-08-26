527dfb16d8154bd2e6e5775f799b1f4081384743
Ankit Mourya <arccra3@gmail.com>
2024-08-26 09:19:50 +0530

    "Updated comment in passbyreference.js to specify changing fuel"

--- Beginner/passbyreference.js
+++ Beginner/passbyreference.js
@@ -2,7 +2,7 @@ let spaceship = {
     'Fuel Type' : 'Turbo Fuel',
     homePlanet : 'Earth'
   };
-  //Creating function to change  
+  //Creating function to change fuel  
   const greenEnergy = obj => {
     obj['Fuel Type'] = 'avocado oil';
   };
