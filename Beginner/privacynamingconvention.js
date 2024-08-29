bce37ae9aec0e813ba79d0591900c3bf89aee151
Ankit Mourya <arccra3@gmail.com>
2024-08-29 11:20:13 +0530

    "Updated comment in recharge function call"

--- Beginner/privacynamingconvention.js
+++ Beginner/privacynamingconvention.js
@@ -5,7 +5,7 @@ const robot = {
       console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
     }
   };
-  //Calling recharge function for
+  //Calling recharge function for testing
   robot.recharge();
   
   
