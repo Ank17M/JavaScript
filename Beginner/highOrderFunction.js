dc3498cc58158d94d3f1bfcdd96b0a68ebb4305d
Ankit Mourya <arccra3@gmail.com>
2024-08-31 10:48:27 +0530

    "Updated comment to add 'two' at the end of the sentence."

--- Beginner/highOrderFunction.js
+++ Beginner/highOrderFunction.js
@@ -1,4 +1,4 @@
-//Creating an object that adds two with 
+//Creating an object that adds two with two
 const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
     for(let i = 1; i <= 1000000; i++) {
       if ( (2 + 2) != 4) {
