ce53c8c892e1de76b3e110a9a71970343044024e
Ankit Mourya <arccra3@gmail.com>
2024-08-31 10:48:18 +0530

    "Update comment in highOrderFunction.js"

--- Beginner/highOrderFunction.js
+++ Beginner/highOrderFunction.js
@@ -1,4 +1,4 @@
-//Creating an object that adds two 
+//Creating an object that adds two with 
 const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
     for(let i = 1; i <= 1000000; i++) {
       if ( (2 + 2) != 4) {
